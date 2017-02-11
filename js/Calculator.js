f90ecc24c7669512c32187b021144b05423f208b
tonytran1 <tonylyfe@gmail.com>
2017-02-11 02:40:44 -0800

    Truncated floating point math
    Added trigonometric functions

--- js/Calculator.js
+++ js/Calculator.js
@@ -1,33 +1,44 @@
 class Calculator {
-  constructor(value) {
-    value = value ? value : 0;
+  constructor(value = 0) {
     this.set(value);
   }
   set(value) {
     this.currentValue = value;
   }
+  get() {
+    return this.currentValue;
+  }
   add(value) {
     this.currentValue += value;
-    return this.currentValue;
+    return this.setPrecision();
   }
   subtract(value) {
     this.currentValue -= value;
-    return this.currentValue;
+    return this.setPrecision();
   }
   multiply(value) {
     this.currentValue *= value;
-    return this.currentValue;
+    return this.setPrecision();
   }
   divide(value) {
     this.currentValue /= value;
-    return this.currentValue;
+    return this.setPrecision();
   }
   power(value) {
     this.currentValue = Math.pow(this.currentValue, value);
-    return this.currentValue;
+    return this.setPrecision();
   }
-  clear() {
-    this.currentValue = 0;
+  sin(value) {
+    return Math.sin(value);
+  }
+  cos(value) {
+    return Math.cos(value);
+  }
+  tan(value) {
+    return Math.tan(value);
+  }
+  setPrecision() {
+    this.currentValue = parseFloat(this.currentValue.toPrecision(15));
     return this.currentValue;
   }
 }
--- js/control.js
+++ js/control.js
@@ -1,6 +1,7 @@
 let calculator = new Calculator();
 let operator = 'none';
 let startNewNumber = false;
+let unit = 'deg';
 let currentValue;
 
 $('.button').on('click', function(event) {
@@ -12,31 +13,43 @@ $('.button').on('click', function(event) {
 
 const events = {
   'clear' : (value) => clearOperations[value](),
-  'operator' : (value) => setOperator(value),
   'number' : (value) => appendToNumber(value),
+  'operator' : (value) => setOperator(value),
+  'trigonometry' : (value) => trigonometry(value),
+  'unit-change' : () => unitChange(),
   'sign-change' : () => signChange(),
   'equals' : () => solve()
 }
 
-const math = {
-  '+' : (value) => calculator.add(value),
-  '-' : (value) => calculator.subtract(value),
-  '/' : (value) => calculator.divide(value),
-  '*' : (value) => calculator.multiply(value),
-  '^' : (value) => calculator.power(value),
-  'none' : (value) => value
-};
+function unitChange() {
+  unit = unit === 'deg' ? 'rad' : 'deg';
+  $('button[data-type="unit-change"]').html(unit);
+}
 
-const clearOperations = {
-  'C' : () => clearAll(),
-  'CE' : () => { $('#input').html('0'); $('#calculation').html(''); },
-  'DEL' : () => removeLastDigit()
-};
+function trigonometry(type) {
+  let value = parseFloat($('#input').html());
+  let radians = (unit === 'deg') ? degreesToRadians(value) : value;
+  if (operator === 'none') {
+    let result = trig[type](radians);
+    calculator.set(result);
+    $('#input').html(result);
+    $('#calculation').html(type + `(${ value })`);
+  } else {
+    let previousResult = calculator.get();
+    let result = trig[type](radians);
+    $('#input').html(result);
+    $('#calculation').html(previousResult + operator + type + `(${ value })`);
+  }
+}
 
-function setOperator(value) {
+function degreesToRadians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
+
+function setOperator(type) {
   let number = parseFloat($('#input').html());
   startNewNumber = true;
-  operator = value;
+  operator = type;
   currentValue = number;
   calculator.set(number);
   $('#input').html('0');
@@ -80,5 +93,27 @@ function removeLastDigit() {
 
 function clearAll() {
   calculator.set(0);
+  operator = 'none';
   $('#input').html('0');
+  $('#calculation').html('');
 }
+
+const math = {
+  '+' : (value) => calculator.add(value),
+  '-' : (value) => calculator.subtract(value),
+  '/' : (value) => calculator.divide(value),
+  '*' : (value) => calculator.multiply(value),
+  '^' : (value) => calculator.power(value),
+  'none' : (value) => value
+};
+
+const trig = {
+  'sin' : (value) => calculator.sin(value),
+  'cos' : (value) => calculator.cos(value),
+  'tan' : (value) => calculator.tan(value)
+}
+
+const clearOperations = {
+  'C' : () => clearAll(),
+  'DEL' : () => removeLastDigit()
+};
